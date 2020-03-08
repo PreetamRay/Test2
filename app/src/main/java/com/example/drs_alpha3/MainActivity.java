60b9ecc1ea0a8e25bcb0e40357d4c93eb411ea90
PreetamRay <preetam.mckv@gmail.com>
2020-03-08 20:30:42 +0530

    added layout

--- app/src/main/java/com/example/drs_alpha3/MainActivity.java
+++ app/src/main/java/com/example/drs_alpha3/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.drs_alpha3;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-    @O
+    @Override
+    public void onCreate (Bundle s){
+        super.onCreate(s);
+        setContentView(R.layout.activity_main);
+    }
 }
